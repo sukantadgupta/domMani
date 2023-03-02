4907d5081cb57575592736cf056812e5cc30dad1
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-03-02 13:09:06 +0530

    2-03-23

---
+++ dom.js
@@ -0,0 +1,14 @@
+document.title = 123;
+
+let header = document.getElementById("main-header");
+
+header.style.borderBottom = "3px solid #000";
+
+let title = document.getElementById("add-items");
+
+console.log(title);
+
+title.style.color = "green";
+
+title.style.fontWeight = "bold";
+
