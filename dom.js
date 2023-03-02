6ea36f1fd6967b66f3f8df1e7fd90c6d7a2ce54f
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-03-02 14:45:51 +0530

    byTagName

--- dom.js
+++ dom.js
@@ -12,13 +12,26 @@ title.style.color = "green";
 
 title.style.fontWeight = "bold";
 
-let items = document.getElementsByClassName("list-group");
-items[2].style.backgroundColor = "green";
+// let items = document.getElementsByClassName("list-group");
+// items[2].style.backgroundColor = "green";
+
+
+// for(let i = 0; i < items.length; i++){
+//     items[i].style.fontWeight = "bold";
+// }
+
+let items = document.getElementsByTagName("li");
+// items[2].style.backgroundColor = "green";
 
 
 for(let i = 0; i < items.length; i++){
     items[i].style.fontWeight = "bold";
 }
+
+var li_five = document.getElementsByClassName("list-five")[0];
+li_five.style.backgroundColor = "green";
+
+
 
 
 
